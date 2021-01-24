23e1ebc589608563d3a067124f717d91ee3cd8d6
Hanqiu Jiang <hanq.jiang@gmail.com>
2021-01-24 15:32:33 +0100

    bugfix: Add cname to prevent reset of custom domain

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 set -e
 
 npm run build
+touch dist/
+echo "hanqiu-jiang.science" > dist/CNAME
 git add -f dist && git commit -m "release: Release dist"
 git push origin `git subtree split --prefix dist master`:gh-pages --force
