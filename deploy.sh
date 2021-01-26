c8337806b19f825213726c64d8a81fa0b4e174a6
Hanqiu Jiang <hanq.jiang@gmail.com>
2021-01-26 20:58:13 +0100

    refactor: Remove webpack

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-npm run build
-touch dist/
-echo "hanqiu-jiang.science" > dist/CNAME
-git add -f dist && git commit -m "release: Release dist"
-git push origin `git subtree split --prefix dist master`:gh-pages --force
